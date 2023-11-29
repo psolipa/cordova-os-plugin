bed0a802c6b414dfacab79f3faa44c28ca871f49
psolipa <86894665+psolipa@users.noreply.github.com>
2023-11-29 11:35:49 +0000

    Update OSPlugin.js

--- www/OSPlugin.js
+++ www/OSPlugin.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 
 var DeviceInfoPlugin = {
     getDeviceInfo: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'DeviceInfoPlugin', 'getDeviceInfo', []);
+        exec(successCallback, errorCallback, 'OSPlugin', 'getDeviceInfo', []);
     }
 };
 
